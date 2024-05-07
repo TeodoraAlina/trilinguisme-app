906b67e7b02847322df7ce1dcdd5d1b9234f9e5e
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-07 22:26:23 +0200

    Setup react-router-dom, create Home and About pages, and add Menu component for navigation

---
+++ trilinguisme-app/src/constants/index.ts
@@ -0,0 +1,5 @@
+export const LANGUAGES = [
+    { label: "Deutsch", code: "de" },
+    { label: "Français", code: "fr" },
+    { label: "Espagnol", code: "es" },
+];
---
+++ trilinguisme-app/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export const Home = () => {
+    return (
+        <main>
+            <h1>Trilinguisme App</h1>
+            <span>Select another language!</span>
+        </main>
+    );
+};
