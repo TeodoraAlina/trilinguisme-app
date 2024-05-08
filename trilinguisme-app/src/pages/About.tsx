4fef5e074968a7c36d8f6d835a792a8cfc6c28c2
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-08 22:09:54 +0200

    Implemented useTranslation hook to enable language switching and added translations to Menu, Home and About

--- trilinguisme-app/src/pages/About.tsx
+++ trilinguisme-app/src/pages/About.tsx
@@ -1,7 +1,10 @@
+import { useTranslation } from "react-i18next";
+
 export const About = () => {
+    const { t } = useTranslation();
     return (
         <main>
-            <h1>About</h1>
+            <h1>{t("about")}</h1>
         </main>
     );
 };
--- trilinguisme-app/src/pages/Home.tsx
+++ trilinguisme-app/src/pages/Home.tsx
@@ -1,8 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 export const Home = () => {
+    const { t } = useTranslation();
+
     return (
         <main>
-            <h1>Trilinguisme App</h1>
-            <span>Select another language!</span>
+            <h1>{t("title")}</h1>
+            <span>{t("label")}</span>
         </main>
     );
 };
