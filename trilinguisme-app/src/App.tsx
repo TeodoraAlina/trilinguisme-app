f86568052e615bff7d65761d29becf477c14f9da
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-09 11:45:41 +0200

    Added Suspense for translations loading

--- trilinguisme-app/src/App.tsx
+++ trilinguisme-app/src/App.tsx
@@ -2,16 +2,20 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { About } from "./pages/About";
 import { Menu } from "./components/Menu";
+import { Suspense } from "react";
 
 const App = () => {
-    return (
-        <BrowserRouter>
-          <Menu />
-          <Routes>
-            <Route path="/" element= {<Home />} />
-            <Route path="/about" element= {<About />} />
-          </Routes>
-        </BrowserRouter>
-    );
-};
-export default App;
+
+  return (
+    <Suspense fallback='loading'>
+      <BrowserRouter>
+        <Menu />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
+  )
+}
+export default App
