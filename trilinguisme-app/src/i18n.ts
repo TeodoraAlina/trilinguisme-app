6132edfdf65e3fb2e8da633cbedb4eb7234bbbb2
TeodoraAlina <115451011+TeodoraAlina@users.noreply.github.com>
2024-05-10 18:10:22 +0200

    Implemented dynamic host detection in i18n file for environment flexibility, added vite-env.d.ts to tsconfig.json includes, and included vite-env.d.ts in gitignore

--- trilinguisme-app/src/i18n.ts
+++ trilinguisme-app/src/i18n.ts
@@ -2,6 +2,8 @@ import i18n from "i18next";
 import i18nBackend from "i18next-http-backend";
 import { initReactI18next } from "react-i18next";
 
+const getCurrentHost = import.meta.env.MODE === 'development' ? 'http://localhost:5173' : 'https://trilinguisme.netlify.app'
+
 i18n
   .use(i18nBackend)
   .use(initReactI18next)
@@ -12,7 +14,7 @@ i18n
       escapeValue: false,
     },
     backend: {
-        loadPath: `${window.location.origin}/i18n/{{lng}}.json`,
+      loadPath: `${getCurrentHost}/i18n/{{lng}}.json`,
     },
   });
 
